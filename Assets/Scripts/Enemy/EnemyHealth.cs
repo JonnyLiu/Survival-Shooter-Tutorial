622f041253e1b44d756d517937ceb45119dd5694
LiuJing <1580786057@qq.com>
2018-05-24 11:03:19 +0800

    梦游射击游戏

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public float sinkSpeed = 2.5f;
+    public int scoreValue = 10;
+    public AudioClip deathClip;//剪辑
+
+
+    Animator anim;
+    AudioSource enemyAudio;//音频
+    ParticleSystem hitParticles;//粒子系统
+    CapsuleCollider capsuleCollider;//碰撞体
+    bool isDead;
+    bool isSinking;
+
+
+    void Awake ()
+    {
+        anim = GetComponent <Animator> ();//获取动画控制器
+        enemyAudio = GetComponent <AudioSource> ();//获取音频
+        hitParticles = GetComponentInChildren <ParticleSystem> ();//获取音频
+        capsuleCollider = GetComponent <CapsuleCollider> ();
+
+        currentHealth = startingHealth;
+    }
+
+
+    void Update ()
+    {
+        if(isSinking)//判断角色死亡
+        {
+            transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);//角色受损 播放音频 减血
+        }
+    }
+
+
+    public void TakeDamage (int amount, Vector3 hitPoint)
+    {
+        if(isDead)
+            return;
+
+        enemyAudio.Play ();
+
+        currentHealth -= amount;
+            
+        hitParticles.transform.position = hitPoint;//粒子特效的位置为碰撞点位置
+        hitParticles.Play();//粒子播放
+
+        if(currentHealth <= 0)//如果生命值小于0死亡
+        {
+            Death ();
+        }
+    }
+
+
+    void Death ()//死亡函数
+    {
+        isDead = true;//死亡为真
+
+        capsuleCollider.isTrigger = true;
+
+        anim.SetTrigger ("Dead");//激活死亡参数
+
+        enemyAudio.clip = deathClip;//播放死亡动画片段
+        enemyAudio.Play ();
+    }
+
+
+    public void StartSinking ()//死亡的僵尸销毁
+    {
+        GetComponent <UnityEngine.AI.NavMeshAgent> ().enabled = false;//关闭寻路组件
+        GetComponent <Rigidbody> ().isKinematic = true;//钢铁动力学开启
+        isSinking = true;//设置isSinking为真
+        ScoreManager.score += scoreValue; //更新ScoreManager文本计算分数
+        Destroy (gameObject, 2f);//销毁 2F后
+    }
+}
---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+    public float range = 100f;
+
+
+    float timer;
+    Ray shootRay = new Ray();
+    RaycastHit shootHit;
+    int shootableMask;
+    ParticleSystem gunParticles;
+    LineRenderer gunLine;//渲染
+    AudioSource gunAudio;
+    Light gunLight;//射线
+    float effectsDisplayTime = 0.2f;
+
+
+    void Awake ()
+    {
+        shootableMask = LayerMask.GetMask ("Shootable");
+        gunParticles = GetComponent<ParticleSystem> ();
+        gunLine = GetComponent <LineRenderer> ();
+        gunAudio = GetComponent<AudioSource> ();//获取某一层 存放在AudioSource
+        gunLight = GetComponent<Light> ();
+    }
+
+
+    void Update ()
+    {
+        timer += Time.deltaTime;
+
+		if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+        {
+            Shoot ();
+        }
+
+        if(timer >= timeBetweenBullets * effectsDisplayTime)
+        {
+            DisableEffects ();
+        }
+    }
+
+
+    public void DisableEffects ()//禁用组件
+    {
+        gunLine.enabled = false;
+        gunLight.enabled = false;
+    }
+
+
+    void Shoot ()
+    {
+        timer = 0f;//射击时间
+
+        gunAudio.Play ();//射击声音
+
+        gunLight.enabled = true;//开启
+
+        gunParticles.Stop ();//射击停止粒子效果
+        gunParticles.Play ();
+
+        gunLine.enabled = true;
+        gunLine.SetPosition (0, transform.position);
+
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+        {
+            EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
+            if(enemyHealth != null)
+            {
+                enemyHealth.TakeDamage (damagePerShot, shootHit.point);
+            }
+            gunLine.SetPosition (1, shootHit.point);
+        }
+        else
+        {
+            gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+}
